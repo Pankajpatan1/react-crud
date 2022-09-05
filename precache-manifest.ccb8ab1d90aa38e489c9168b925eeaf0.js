0d9be834ca5a9fa8c506f9695063a2b292b69e4a
Pankajpatan1 <pankaj@reckonsys.com>
2022-09-05 11:55:29 +0530

    Updates

---
+++ precache-manifest.ccb8ab1d90aa38e489c9168b925eeaf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc7846e5d1f1ba863f94645abc98ba3",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "c23b40932b51fc4fc426",
+    "url": "/react-crud/static/css/main.f23e3a2e.chunk.css"
+  },
+  {
+    "revision": "a9115227551086b13256",
+    "url": "/react-crud/static/js/2.78c054e9.chunk.js"
+  },
+  {
+    "revision": "1ca36cbe8e384e52ec22bd08211853ad",
+    "url": "/react-crud/static/js/2.78c054e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23b40932b51fc4fc426",
+    "url": "/react-crud/static/js/main.8a702b67.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/react-crud/static/js/main.8a702b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947da9bb40ba37cd54c6",
+    "url": "/react-crud/static/js/runtime-main.cac5419d.js"
+  }
+]);
